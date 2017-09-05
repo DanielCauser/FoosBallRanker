df75fc2c0fc47e96006d8aab966b139ef5e889fd
Daniel John Causer <DanielCauser@users.noreply.github.com>
2017-09-05 10:35:47 -0400

    Adding layout for login page.
    This still needs to be separated from the main page.

--- FoosBallRanker/FoosBallRanker/obj/Debug/FoosBallRanker.Views.MainPage.xaml.g.cs
+++ FoosBallRanker/FoosBallRanker/obj/Debug/FoosBallRanker.Views.MainPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace FoosBallRanker.Views {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Image BgImage;
+        
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             this.LoadFromXaml(typeof(MainPage));
+            BgImage = this.FindByName<global::Xamarin.Forms.Image>("BgImage");
         }
     }
 }
