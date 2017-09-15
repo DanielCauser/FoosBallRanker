34197ad6db69bbe15535962d6b2d423d34c35297
Daniel John Causer <danielcauser@gmail.com>
2017-09-14 22:13:14 -0400

    Facebook login service

---
+++ FoosBallRanker/FoosBallRanker/Services/Authentication/FacebookLoginService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace FoosBallRanker.Services.Authentication
+{
+    public class FacebookLoginService
+    {
+        public FacebookLoginService()
+        {
+        }
+    }
+}
